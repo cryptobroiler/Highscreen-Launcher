1f67275f8b1af9e0999c7a77f8f8e39f29286f50
Dennis Kopin <dennis.kopin@gmail.com>
2010-11-11 18:25:48 +0300

    Added default apps list from HIGHSCREEN Alex
    Signed-off-by: Dennis Kopin <dennis.kopin@gmail.com>

--- src/org/highscreen/launcher/ItemFactory.java
+++ src/org/highscreen/launcher/ItemFactory.java
@@ -14,125 +14,175 @@ import org.highscreen.launcher.items.AbstractItem;
 import org.highscreen.launcher.items.StandardItem;
 
 /**
- * Created by IntelliJ IDEA.
- * User: ex3ndr
- * Date: 13.07.2010
- * Time: 4:26:50
- * To change this template use File | Settings | File Templates.
+ * Created by IntelliJ IDEA. User: ex3ndr Date: 13.07.2010 Time: 4:26:50 To
+ * change this template use File | Settings | File Templates.
  */
 public class ItemFactory {
 
-    public static final String[] BLOCKED = new String[]{
-            "com.bravo.ereader.activities.ReaderActivity",
-            "com.bravo.reader.instore.ReaderActivity",
-            "com.ereader.android.nook.ReaderActivity",
-            "com.android.development.Development",
-            "com.bravo.FormatSdcard",
-            "com.bravo.app.log.LogSettings",
-          //  "org.geometerplus.android.fbreader.FBReader",
-            "org.highscreen.launcher.LauncherActivity",
-    };
+	public static final String[] BLOCKED = new String[] {
+			"com.bravo.ereader.activities.ReaderActivity",
+			"com.bravo.reader.instore.ReaderActivity",
+			"com.ereader.android.nook.ReaderActivity",
+			"com.android.development.Development", "com.bravo.FormatSdcard",
+			"com.bravo.app.log.LogSettings",
+			// "org.geometerplus.android.fbreader.FBReader",
+			"org.highscreen.launcher.LauncherActivity", };
 
-    public static boolean isBlocked(String name) {
-        for (int i = 0; i < BLOCKED.length; i++)
-            if (BLOCKED[i].equals(name))
-                return true;
+	public static boolean isBlocked(String name) {
+		for (int i = 0; i < BLOCKED.length; i++)
+			if (BLOCKED[i].equals(name))
+				return true;
 
-        return false;
-    }
+		return false;
+	}
 
-    private static ItemsStorage.Item[] getAllItems(Context context) {
-        ArrayList<ItemsStorage.Item> items = new ArrayList<ItemsStorage.Item>();
+	private static ItemsStorage.Item[] getAllItems(Context context) {
+		ArrayList<ItemsStorage.Item> items = new ArrayList<ItemsStorage.Item>();
 
-        PackageManager pkgManager = context.getPackageManager();
+		PackageManager pkgManager = context.getPackageManager();
 
-        Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
-        mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
-        List<ResolveInfo> installed = pkgManager.queryIntentActivities(mainIntent, 0);
+		Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
+		mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+		List<ResolveInfo> installed = pkgManager.queryIntentActivities(
+				mainIntent, 0);
 
-        for (ResolveInfo i : installed) {
-            if (isBlocked(i.activityInfo.name))
-                continue;
-            Log.d("NOOK:",i.activityInfo.name);
-            items.add(new ItemsStorage.Item(AbstractItem.TYPE_STANDARD, Intent.ACTION_MAIN + ":" + i.activityInfo.packageName + ":" + i.activityInfo.name));
-        }
+		for (ResolveInfo i : installed) {
+			if (isBlocked(i.activityInfo.name))
+				continue;
+			Log.d("NOOK:", Intent.ACTION_MAIN + ":"
+					+ i.activityInfo.packageName + ":" + i.activityInfo.name);
+			items.add(new ItemsStorage.Item(AbstractItem.TYPE_STANDARD,
+					Intent.ACTION_MAIN + ":" + i.activityInfo.packageName + ":"
+							+ i.activityInfo.name));
+		}
 
-//        items.add(new ItemsStorage.Item(AbstractItem.TYPE_READING_NOW, ""));
-//
-//items.add(new ItemsStorage.Item(AbstractItem.TYPE_WIFI, ""));
-items.add(new ItemsStorage.Item(AbstractItem.TYPE_WIFI_TOGGLE, ""));
+		// items.add(new ItemsStorage.Item(AbstractItem.TYPE_READING_NOW, ""));
+		//
+		// items.add(new ItemsStorage.Item(AbstractItem.TYPE_WIFI, ""));
+		items.add(new ItemsStorage.Item(AbstractItem.TYPE_WIFI_TOGGLE, ""));
 
-        return items.toArray(new ItemsStorage.Item[0]);
-    }
+		return items.toArray(new ItemsStorage.Item[0]);
+	}
 
-    private static ItemsStorage.Item[] getDefault() {
-        return new ItemsStorage.Item[]{
-                new ItemsStorage.Item(AbstractItem.TYPE_STANDARD, "android.intent.action.MAIN:com.android.settings:com.android.settings.Settings"),
-                
-        };
-    }
+	private static ItemsStorage.Item[] getDefault() {
+		return new ItemsStorage.Item[] {
+				new ItemsStorage.Item(AbstractItem.TYPE_STANDARD,
+						"android.intent.action.MAIN:com.android.settings:com.android.settings.Settings"),
+				new ItemsStorage.Item(AbstractItem.TYPE_WIFI_TOGGLE, ""),
+				new ItemsStorage.Item(
+						AbstractItem.TYPE_STANDARD,
+						"android.intent.action.MAIN:com.android.email:com.android.email.activity.Welcome"),
+				new ItemsStorage.Item(
+						AbstractItem.TYPE_STANDARD,
+						"android.intent.action.MAIN:com.android.camera:com.android.camera.GalleryPicker"),
+				new ItemsStorage.Item(
+						AbstractItem.TYPE_STANDARD,
+						"android.intent.action.MAIN:com.android.browser:com.android.browser.BrowserActivity"),
+				new ItemsStorage.Item(
+						AbstractItem.TYPE_STANDARD,
+						"android.intent.action.MAIN:com.android.music:com.android.music.MusicBrowserActivity"),
+				new ItemsStorage.Item(
+						AbstractItem.TYPE_STANDARD,
+						"android.intent.action.MAIN:alex.reader.ebook.viewer:alex.reader.ebook.viewer.activity.MainMenuActivity"),
+				new ItemsStorage.Item(
+						AbstractItem.TYPE_STANDARD,
+						"android.intent.action.MAIN:alex.reader.ebook.viewer:alex.reader.ebook.viewer.activity.About"),
+				new ItemsStorage.Item(
+						AbstractItem.TYPE_STANDARD,
+						"android.intent.action.MAIN:com.android.calculator2:com.android.calculator2.Calculator"),
+				new ItemsStorage.Item(
+						AbstractItem.TYPE_STANDARD,
+						"android.intent.action.MAIN:com.funkyandroid.appstore.client.alex:com.funkyandroid.appstore.client.alex.AppStoreClient"),
+				new ItemsStorage.Item(AbstractItem.TYPE_STANDARD,
+						"android.intent.action.MAIN:com.mobilein.kldsp:com.mobilein.kldsp.MainActivity"),
+				new ItemsStorage.Item(
+						AbstractItem.TYPE_STANDARD,
+						"android.intent.action.MAIN:org.geometerplus.zlibrary.ui.android:org.geometerplus.android.fbreader.FBReader"),
+				new ItemsStorage.Item(AbstractItem.TYPE_STANDARD,
+						"android.intent.action.MAIN:com.mobilein.kom:com.mobilein.kom.MainActivity"),
+				new ItemsStorage.Item(AbstractItem.TYPE_STANDARD,
+						"android.intent.action.MAIN:com.mobilein.mk:com.mobilein.mk.MainActivity"),
+				new ItemsStorage.Item(
+						AbstractItem.TYPE_STANDARD,
+						"android.intent.action.MAIN:com.imobilco.alex.app:com.imobilco.alex.app.AlexActivity"),
+				new ItemsStorage.Item(AbstractItem.TYPE_STANDARD,
+						"android.intent.action.MAIN:com.mobilein.trud:com.mobilein.trud.MainActivity"),
+				new ItemsStorage.Item(AbstractItem.TYPE_STANDARD,
+						"android.intent.action.MAIN:com.mobilein.news:com.mobilein.news.MainActivity"),
+				new ItemsStorage.Item(AbstractItem.TYPE_STANDARD,
+						"android.intent.action.MAIN:com.mobilein.eg:com.mobilein.eg.MainActivity"),
+				new ItemsStorage.Item(AbstractItem.TYPE_STANDARD,
+						"android.intent.action.MAIN:com.mobilein.aif:com.mobilein.aif.MainActivity"),
+				new ItemsStorage.Item(
+						AbstractItem.TYPE_STANDARD,
+						"android.intent.action.MAIN:org.openintents.filemanager:org.openintents.filemanager.FileManagerActivity"),
+				new ItemsStorage.Item(
+						AbstractItem.TYPE_STANDARD,
+						"android.intent.action.MAIN:com.cbaplab.bogr:com.cbaplab.bogr.activity.QuotesList"),
 
-    private static ItemsStorage.Item[] getSelectedItems(Context context) {
-        ItemsStorage.Item[] items = ItemsStorage.loadItems(context);
-        Log.d("NOOK",String.valueOf(items.length));
-        if (items.length == 0)
-            items = getDefault();
+		};
+	}
 
-        return items;
-    }
+	private static ItemsStorage.Item[] getSelectedItems(Context context) {
+		ItemsStorage.Item[] items = ItemsStorage.loadItems(context);
+		Log.d("Alex Launcher", String.valueOf(items.length));
+		if (items.length == 0)
+			items = getDefault();
 
-    private static ItemsStorage.Item[] getUnselectedItems(Context context) {
-        ArrayList<ItemsStorage.Item> items = new ArrayList<ItemsStorage.Item>();
+		return items;
+	}
 
-        ItemsStorage.Item[] allItems = getAllItems(context);
-        ItemsStorage.Item[] selected = getSelectedItems(context);
+	private static ItemsStorage.Item[] getUnselectedItems(Context context) {
+		ArrayList<ItemsStorage.Item> items = new ArrayList<ItemsStorage.Item>();
 
-        outer:
-        for (int a = 0; a < allItems.length; a++) {
-            for (int s = 0; s < selected.length; s++) {
-                if (selected[s].equals(allItems[a]))
-                    continue outer;
-            }
+		ItemsStorage.Item[] allItems = getAllItems(context);
+		ItemsStorage.Item[] selected = getSelectedItems(context);
 
-            items.add(allItems[a]);
-        }
+		outer: for (int a = 0; a < allItems.length; a++) {
+			for (int s = 0; s < selected.length; s++) {
+				if (selected[s].equals(allItems[a]))
+					continue outer;
+			}
 
-        return items.toArray(new ItemsStorage.Item[0]);
-    }
+			items.add(allItems[a]);
+		}
 
-    public static List<AbstractItem> loadUnselectedItems(Context context) {
-        ItemsStorage.Item[] items = getUnselectedItems(context);
+		return items.toArray(new ItemsStorage.Item[0]);
+	}
 
-        ArrayList<AbstractItem> res = new ArrayList<AbstractItem>();
-        for (ItemsStorage.Item i : items) {
-            res.add(AbstractItem.createItem(i.data, i.id, context));
-        }
-        return res;
-    }
+	public static List<AbstractItem> loadUnselectedItems(Context context) {
+		ItemsStorage.Item[] items = getUnselectedItems(context);
 
-    public static List<AbstractItem> loadItems(Context context) {
-        ItemsStorage.Item[] items = getSelectedItems(context);
+		ArrayList<AbstractItem> res = new ArrayList<AbstractItem>();
+		for (ItemsStorage.Item i : items) {
+			res.add(AbstractItem.createItem(i.data, i.id, context));
+		}
+		return res;
+	}
 
-        ArrayList<AbstractItem> res = new ArrayList<AbstractItem>();
-        for (ItemsStorage.Item i : items) {
-            AbstractItem item = AbstractItem.createItem(i.data, i.id, context);
-            if (item instanceof StandardItem) {
-                if (((StandardItem) item).isWorks()) {
-                    res.add(item);
-                }
-            } else
-                res.add(item);
-        }
-        return res;
-    }
+	public static List<AbstractItem> loadItems(Context context) {
+		ItemsStorage.Item[] items = getSelectedItems(context);
 
-    public static void saveItems(List<AbstractItem> items, Context context) {
-        ItemsStorage.Item[] res = new ItemsStorage.Item[items.size()];
-        for (int i = 0; i < res.length; i++) {
-            AbstractItem item = items.get(i);
-            res[i] = new ItemsStorage.Item(item.getType(), item.getData());
-        }
+		ArrayList<AbstractItem> res = new ArrayList<AbstractItem>();
+		for (ItemsStorage.Item i : items) {
+			AbstractItem item = AbstractItem.createItem(i.data, i.id, context);
+			if (item instanceof StandardItem) {
+				if (((StandardItem) item).isWorks()) {
+					res.add(item);
+				}
+			} else
+				res.add(item);
+		}
+		return res;
+	}
 
-        ItemsStorage.saveItems(res, context);
-    }
+	public static void saveItems(List<AbstractItem> items, Context context) {
+		ItemsStorage.Item[] res = new ItemsStorage.Item[items.size()];
+		for (int i = 0; i < res.length; i++) {
+			AbstractItem item = items.get(i);
+			res[i] = new ItemsStorage.Item(item.getType(), item.getData());
+		}
+
+		ItemsStorage.saveItems(res, context);
+	}
 }
